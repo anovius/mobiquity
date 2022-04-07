416697d2fe6d89f9b17cbc78d9b88c9d8ffcc4dd
Usman Farooq <anovius10@gmail.com>
2022-04-07 22:55:15 +0500

    fixes

--- apps/otp/src/app/app.component.ts
+++ apps/otp/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'mobiquity-root',
@@ -7,4 +8,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'otp';
+
+  constructor(private router: Router) {}
+
+  goBack(){
+    this.router.navigate([".."]);
+  }
 }
--- apps/reset-pin/src/app/app.component.ts
+++ apps/reset-pin/src/app/app.component.ts
@@ -1,4 +1,6 @@
+import { Location } from '@angular/common';
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'mobiquity-root',
@@ -7,4 +9,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'reset-pin';
+
+  constructor(private router: Router, private location: Location) { }
+
+  goBack(){
+    window.history.back();
+  }
 }
