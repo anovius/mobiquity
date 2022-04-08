91c4758ef048f5546ee1470a312a082d3f8b4dbb
MuhammadFeezan <mfeezanlhr@gmail.com>
2022-04-09 01:27:17 +0500

    half register done

--- apps/register/src/app/address/address.component.ts
+++ apps/register/src/app/address/address.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { TranslationService } from '@mobiquity/shared';
 
 @Component({
   selector: 'mobiquity-address',
   templateUrl: './address.component.html',
-  styleUrls: ['./address.component.css']
+  styleUrls: ['./address.component.css'],
 })
 export class AddressComponent implements OnInit {
+  translation: any;
 
-  constructor() { }
+  constructor(private translationService: TranslationService) {}
 
-  ngOnInit(): void {
+  ngOnInit() {
+    this.translationService.get().subscribe((data: any) => {
+      this.translation = data.register;
+      console.log(this.translation);
+    });
   }
-
 }
