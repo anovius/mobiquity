bfec4319f985a0287cbcedae1712b803c85ba4c4
MuhammadFeezan <mfeezanlhr@gmail.com>
2022-04-09 02:42:50 +0500

    4 by 5th done

--- apps/register/src/app/kyc-detail-success/kyc-detail-success.component.ts
+++ apps/register/src/app/kyc-detail-success/kyc-detail-success.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { TranslationService } from '@mobiquity/shared';
 
 @Component({
   selector: 'mobiquity-kyc-detail-success',
   templateUrl: './kyc-detail-success.component.html',
-  styleUrls: ['./kyc-detail-success.component.css']
+  styleUrls: ['./kyc-detail-success.component.css'],
 })
 export class KycDetailSuccessComponent implements OnInit {
+  translation: any;
 
-  constructor() { }
+  constructor(private translationService: TranslationService) {}
 
-  ngOnInit(): void {
+  ngOnInit() {
+    this.translationService.get().subscribe((data: any) => {
+      this.translation = data.register;
+    });
   }
-
 }
