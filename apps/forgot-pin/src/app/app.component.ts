79cba88c20ace88eafc75a12ba81eaedb8866937
Usman Farooq <anovius10@gmail.com>
2022-04-08 06:17:03 +0500

    services setup done

--- apps/forgot-pin/src/app/app.component.ts
+++ apps/forgot-pin/src/app/app.component.ts
@@ -1,12 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { TranslationService } from '@mobiquity/shared';
 
 @Component({
   selector: 'mobiquity-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'forgot-pin';
+
+  constructor(
+    private translationService: TranslationService
+  ) {}
+
+  ngOnInit() {
+    
+  }
 
   goBack(){
     window.history.back();
