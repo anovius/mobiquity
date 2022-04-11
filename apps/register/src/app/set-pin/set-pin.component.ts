5b61b705de79338b569146858facde6f5d81342b
MuhammadFeezan <mfeezanlhr@gmail.com>
2022-04-11 20:53:27 +0500

    last part done

--- apps/register/src/app/set-pin/set-pin.component.ts
+++ apps/register/src/app/set-pin/set-pin.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { TranslationService } from '@mobiquity/shared';
 
 @Component({
   selector: 'mobiquity-set-pin',
   templateUrl: './set-pin.component.html',
-  styleUrls: ['./set-pin.component.css']
+  styleUrls: ['./set-pin.component.css'],
 })
 export class SetPinComponent implements OnInit {
+  translation: any;
 
-  constructor() { }
+  constructor(private translationService: TranslationService) {}
 
-  ngOnInit(): void {
+  ngOnInit() {
+    this.translationService.get().subscribe((data: any) => {
+      this.translation = data.register;
+    });
   }
-
 }
